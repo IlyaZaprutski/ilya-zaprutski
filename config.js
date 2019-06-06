337bfe199550d797dd297d25d8f740e6d38810c0
Ilya Zaprutski <ilya.zaprutski@gmail.com>
2019-06-06 18:12:21 +0300

    test split

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-highlights/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav',
+  heading: 'test split',
   subHeading: 'Web Developer',
   // social
   socialLinks: [
