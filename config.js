82266810321c21bed42ee170a0864c52451c7754
Ilya Zaprutski <ilya.zaprutski@gmail.com>
2019-06-06 18:14:25 +0300

    update subHeading

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-highlights/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Ilya Zaprutski',
-  subHeading: 'Web Developer',
+  subHeading: 'Developer',
   // social
   socialLinks: [
     {
